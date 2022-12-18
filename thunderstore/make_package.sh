684c7bf3e965d77d888ff84f253b10c50d1146a5
Ash Ether <ash.zsh@proton.me>
2022-12-18 12:34:45 -0600

    workflow restructure

---
+++ thunderstore/make_package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export TS_DIR="$(dirname "${BASH_SOURCE[0]}")"
+export VERSION=$(git describe --tags --abbrev=0 | sed -n 's/v\(.\+\)/\1/p')
+cd $TS_DIR
+
+# Delete the existing build if it exists
+rm Wristimate.zip
+
+# Create our temp folders
+mkdir -p TEMP/
+
+# Copy the files into them
+cp manifest.json TEMP/manifest.json
+cp icon.png TEMP/icon.png
+cp ../README.md TEMP/README.md
+cp ../src/Wristimate/bin/Release/net35/Wristimate.dll TEMP/Wristimate.dll
+
+# Modify the version number
+sed -i "s/{VERSION}/$VERSION/g" TEMP/manifest.json
+
+# Zip the folder
+cd TEMP
+zip -9r ../Wristimate.zip *
+
+# Delete the temp dir
+cd ..
+rm -r TEMP
